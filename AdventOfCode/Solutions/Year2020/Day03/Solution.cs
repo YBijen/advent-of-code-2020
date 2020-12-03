05620875b508abe16c381206840ee824ac642385
Youri Bijen <YouriBijen@hotmail.com>
2020-12-03 18:00:20 +0100

    Added day 3 solution

--- AdventOfCode/Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day03/Solution.cs
@@ -1,26 +1,72 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day03 : ASolution
+    class Day03Solution : ASolution
     {
-
-        public Day03() : base(03, 2020, "")
+        public Day03Solution() : base(03, 2020, "")
         {
+            //SetDebugInput();
+        }
 
+        private void SetDebugInput()
+        {
+            base.DebugInput =
+                "..##.......\n" +
+                "#...#...#..\n" +
+                ".#....#..#.\n" +
+                "..#.#...#.#\n" +
+                ".#...##..#.\n" +
+                "..#.##.....\n" +
+                ".#.#.#....#\n" +
+                ".#........#\n" +
+                "#.##...#...\n" +
+                "#...##....#\n" +
+                ".#..#...#.#";
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return CountEncounteredTrees(3, 1).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var slopeAngleModifiers = new List<(int X, int Y)>
+            {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+            };
+
+            return slopeAngleModifiers
+                .Select(modifier => CountEncounteredTrees(modifier.X, modifier.Y))
+                .Aggregate((value1, value2) => value1 * value2)
+                .ToString();
+        }
+
+        private long CountEncounteredTrees(int xModifier, int yModifier)
+        {
+            var input = base.Input.SplitByNewline();
+            var singleInputLength = input[0].Length;
+            var amountOfLoops = input.Length / yModifier;
+
+            long encounteredTrees = 0;
+            for (var loop = 0; loop < amountOfLoops; loop++)
+            {
+                var y = loop * yModifier;
+                var x = (loop * xModifier) % singleInputLength;
+                if (input[y][x] == '#')
+                {
+                    encounteredTrees++;
+                }
+            }
+
+            return encounteredTrees;
         }
     }
 }
