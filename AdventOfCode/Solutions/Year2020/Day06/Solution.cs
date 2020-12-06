970dfdaf49922fcd56ea613a1adf4ca3a56f1203
Youri Bijen <youribijen@hotmail.com>
2020-12-06 16:25:20 +0100

    Resolved day 6

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,26 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day06 : ASolution
     {
+        public Day06() : base(06, 2020, "") { }
 
-        public Day06() : base(06, 2020, "")
-        {
-
-        }
-
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
+        protected override string SolvePartOne() => GetAnswersFromInput()
+            .SelectMany(GetDistinctAnswers)
+            .Count()
+            .ToString();
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var total = 0;
+            foreach(var groupAnswers in GetAnswersFromInput())
+            {
+                var personsWhoAnswered = groupAnswers.Split('\n');
+                if(personsWhoAnswered.Length == 1)
+                {
+                    total += personsWhoAnswered[0].Count();
+                    continue;
+                }
+
+                foreach(var distinctAnswer in GetDistinctAnswers(groupAnswers))
+                {
+                    if(groupAnswers.Count(ga => ga == distinctAnswer) == personsWhoAnswered.Length)
+                    {
+                        total++;
+                    }
+                }
+            }
+            return total.ToString();
         }
+
+        private string[] GetAnswersFromInput() => base.Input.Split("\n\n");
+
+        private IEnumerable<char> GetDistinctAnswers(string answers) => answers.Replace("\n", "").Distinct();
+
     }
 }
