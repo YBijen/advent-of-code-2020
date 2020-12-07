3296e506ae01fce9d9c4b02b610150857d5341a4
Youri Bijen <youribijen@hotmail.com>
2020-12-07 17:59:19 +0100

    Finished day 7 part 2

--- AdventOfCode/Solutions/Year2020/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day07/Solution.cs
@@ -9,37 +9,51 @@ namespace AdventOfCode.Solutions.Year2020
     {
         private const string BAG_SHINY_GOLD = "shiny gold";
 
+        private readonly Dictionary<string, Dictionary<string, int>> _luggageRules;
+
         public Day07() : base(07, 2020, "")
         {
-            //SetDebugInput();
+            _luggageRules = ParseLuggageRules();
         }
 
-        private void SetDebugInput()
+        private void SetDebugInputPart1()
         {
             base.DebugInput = "" +
-            "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
-            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
-            "bright white bags contain 1 shiny gold bag.\n" +
-            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
-            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
-            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
-            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
-            "faded blue bags contain no other bags.\n" +
-            "dotted black bags contain no other bags.";
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+                "bright white bags contain 1 shiny gold bag.\n" +
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
+                "faded blue bags contain no other bags.\n" +
+                "dotted black bags contain no other bags.";
+        }
+
+        private void SetDebugInputPart2()
+        {
+            base.DebugInput = "" +
+                "shiny gold bags contain 2 dark red bags.\n" +
+                "dark red bags contain 2 dark orange bags.\n" +
+                "dark orange bags contain 2 dark yellow bags.\n" +
+                "dark yellow bags contain 2 dark green bags.\n" +
+                "dark green bags contain 2 dark blue bags.\n" +
+                "dark blue bags contain 2 dark violet bags.\n" +
+                "dark violet bags contain no other bags.";
         }
 
         protected override string SolvePartOne()
         {
-            var parsedRules = ParseLuggageRules();
-
+            //SetDebugInputPart1();
             var checkedBags = 0;
             var alreadyChecked = new List<string>();
             var bagsToFind = new List<string> { BAG_SHINY_GOLD };
             while(bagsToFind.Count > 0)
             {
                 var currentBag = bagsToFind.First();
+                bagsToFind.RemoveAt(0);
 
-                foreach(var rule in parsedRules)
+                foreach (var rule in _luggageRules)
                 {
                     if(rule.Value.ContainsKey(currentBag) && !bagsToFind.Contains(rule.Key) && !alreadyChecked.Contains(rule.Key))
                     {
@@ -49,10 +63,28 @@ namespace AdventOfCode.Solutions.Year2020
                     }
                 }
 
-                bagsToFind.RemoveAt(0);
             }
 
             return checkedBags.ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            var bagsInGivenBag = CountBagsInBag(BAG_SHINY_GOLD);
+            return (bagsInGivenBag - 1).ToString();
+        }
+
+        private int CountBagsInBag(string bag)
+        {
+            //var count = 0;
+            //var containedBags = _luggageRules[bag];
+            //foreach(var b in containedBags)
+            //{
+            //    count += CountBagsInBag(b.Key) * b.Value;
+            //}
+
+            var amountOfBagsInContainedBags = _luggageRules[bag].Sum(b => CountBagsInBag(b.Key) * b.Value);
+            return amountOfBagsInContainedBags > 0 ? amountOfBagsInContainedBags + 1 : 1;
         }
 
         private Dictionary<string, Dictionary<string, int>> ParseLuggageRules()
@@ -82,10 +114,5 @@ namespace AdventOfCode.Solutions.Year2020
             rule.Replace("contain", "").Replace("bags", "").Replace("bag", "").Replace("no other", "").Replace(",", "").Replace(".", "");
 
         private string[] SplitRule(string rule) => rule.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-        protected override string SolvePartTwo()
-        {
-            return null;
-        }
     }
 }
