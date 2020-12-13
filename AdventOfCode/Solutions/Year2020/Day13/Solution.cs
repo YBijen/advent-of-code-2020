6b13701e58505ba9f52e88f8aaa74a201e5be1d5
Youri Bijen <YouriBijen@hotmail.com>
2020-12-13 13:42:13 +0100

    Resolved day 13 part 1

--- AdventOfCode/Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day13/Solution.cs
@@ -1,26 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day13 : ASolution
     {
+        private readonly int _takeBusAtTime = 0;
+        private readonly List<int> _busIds = new List<int>();
 
         public Day13() : base(13, 2020, "")
         {
-
+            //SetDebugInput();
+            var input = base.Input.SplitByNewline();
+            _takeBusAtTime = int.Parse(input[0]);
+            _busIds = input[1].Split(',').Where(id => id != "x").Select(id => int.Parse(id)).ToList();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            (int busId, int timeToWait) shortestWaitTime = (0, _takeBusAtTime);
+            foreach(var currentBusId in _busIds)
+            {
+                var timeToWait = currentBusId - (_takeBusAtTime % currentBusId);
+                if(timeToWait < shortestWaitTime.timeToWait)
+                {
+                    shortestWaitTime = (currentBusId, timeToWait);
+                }
+            }
+            return (shortestWaitTime.busId * shortestWaitTime.timeToWait).ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        private void SetDebugInput()
+        {
+            base.DebugInput = "939\n7,13,x,x,59,x,31,19";
         }
     }
 }
