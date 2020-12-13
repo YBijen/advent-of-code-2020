192b99e6840355e0c6156896e78a4cf78f8d5e31
Youri Bijen <YouriBijen@hotmail.com>
2020-12-13 16:42:48 +0100

    WIP Day 13 part 2

--- AdventOfCode/Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day13/Solution.cs
@@ -1,28 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode.Solutions.Year2020
 {
     class Day13 : ASolution
     {
-        private readonly int _takeBusAtTime = 0;
-        private readonly List<int> _busIds = new List<int>();
+        private int _takeBusAtTime = 0;
+        private List<(int busId, int departOffset)> _busIds = new List<(int busId, int departOffset)>();
 
         public Day13() : base(13, 2020, "")
         {
-            //SetDebugInput();
-            var input = base.Input.SplitByNewline();
-            _takeBusAtTime = int.Parse(input[0]);
-            _busIds = input[1].Split(',').Where(id => id != "x").Select(id => int.Parse(id)).ToList();
+            //AssertDebugInputsPart2();
+            SetGlobalsFromInput();
         }
 
         protected override string SolvePartOne()
         {
             (int busId, int timeToWait) shortestWaitTime = (0, _takeBusAtTime);
-            foreach(var currentBusId in _busIds)
+            foreach(var currentBus in _busIds)
             {
+                var currentBusId = currentBus.busId;
                 var timeToWait = currentBusId - (_takeBusAtTime % currentBusId);
                 if(timeToWait < shortestWaitTime.timeToWait)
                 {
@@ -34,12 +35,86 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            Console.WriteLine($"Starting part 2...");
+            var sw = Stopwatch.StartNew();
+            var validTimes = 1;
+            long currentTime = 100000000000000;
+            while(validTimes < _busIds.Count)
+            {
+                validTimes = 1;
+                currentTime += _busIds[0].busId;
+                foreach (var currentBus in _busIds.Skip(1).AsParallel())
+                {
+                    var currentBusId = currentBus.busId;
+                    var timeToWait = currentBusId - (currentTime % currentBusId);
+                    if(currentBus.departOffset == timeToWait)
+                    {
+                        validTimes++;
+                    }
+                }
+            }
+            Console.WriteLine($"Took {sw.Elapsed}");
+            return currentTime.ToString();
         }
 
-        private void SetDebugInput()
+        private void SetGlobalsFromInput()
         {
-            base.DebugInput = "939\n7,13,x,x,59,x,31,19";
+            var input = base.Input.SplitByNewline();
+            _takeBusAtTime = int.Parse(input[0]);
+            _busIds.Clear();
+            var splitInput = input[1].Split(',');
+            for (var i = 0; i < splitInput.Length; i++)
+            {
+                if(splitInput[i] == "x")
+                {
+                    continue;
+                }
+                _busIds.Add((int.Parse(splitInput[i]), i));
+            }
+        }
+
+        private void SetDebugInput(int inputNumber)
+        {
+            switch(inputNumber)
+            {
+                case 0:
+                    base.DebugInput = "939\n7,13,x,x,59,x,31,19"; // 1068781
+                    break;
+                case 1:
+                    base.DebugInput = "0\n17,x,13,19"; // 3417
+                    break;
+                case 2:
+                    base.DebugInput = "0\n67,7,59,61"; // 754018
+                    break;
+                case 3:
+                    base.DebugInput = "0\n67,x,7,59,61"; // 779210
+                    break;
+                case 4:
+                    base.DebugInput = "0\n67,7,x,59,61"; // 1261476
+                    break;
+                case 5:
+                    base.DebugInput = "0\n1789,37,47,1889"; // 1202161486
+                    break;
+                default:
+                    throw new Exception("The input number is invalid");
+            }
+        }
+
+        private void AssertDebugInputsPart2()
+        {
+            UseInputAndExpectedResult(0, "1068781");
+            UseInputAndExpectedResult(1, "3417");
+            UseInputAndExpectedResult(2, "754018");
+            UseInputAndExpectedResult(3, "779210");
+            UseInputAndExpectedResult(4, "1261476");
+            UseInputAndExpectedResult(5, "1202161486");
+        }
+
+        private void UseInputAndExpectedResult(int input, string expectedResult)
+        {
+            SetDebugInput(input);
+            SetGlobalsFromInput();
+            Assert.AreEqual(SolvePartTwo(), expectedResult);
         }
     }
 }
