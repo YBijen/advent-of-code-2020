52a0ac89cbfa7d50ae51fe070af5334c29788293
Youri Bijen <youribijen@hotmail.com>
2020-12-15 18:35:46 +0100

    Solved day 15 part 2

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,11 +10,14 @@ namespace AdventOfCode.Solutions.Year2020
     class Day15 : ASolution
     {
         private List<int> _startingNumbers;
+        private Dictionary<int, int[]> _numberIndexCollection;
+
         private List<int> _game;
 
         public Day15() : base(15, 2020, "")
         {
             AssertDebugInputsPart1();
+            AssertDebugInputsPart2();
 
             // My input
             _startingNumbers = new List<int> { 12, 1, 16, 3, 11, 0 };
@@ -23,13 +27,74 @@ namespace AdventOfCode.Solutions.Year2020
         {
             const int AMOUNT_OF_TURNS = 2020;
 
+            var sw = Stopwatch.StartNew();
+            
             _game = new List<int>(_startingNumbers);
 
             for (var turnCount = _startingNumbers.Count; turnCount < AMOUNT_OF_TURNS; turnCount++)
             {
                 _game.Add(GetNextNumber());
             }
-            return _game.Last().ToString();
+
+            var result = _game.Last().ToString();
+            sw.Stop();
+            Console.WriteLine($"Solution 1 solved in {sw.Elapsed}.");
+
+
+
+            sw.Restart();
+
+            var currentTurn = 1;
+            _numberIndexCollection = new Dictionary<int, int[]>();
+            _startingNumbers.ForEach(number => _numberIndexCollection.Add(number, new int[2] { currentTurn++, -1 }));
+
+            var lastAdded = _startingNumbers.Last();
+            while (currentTurn <= AMOUNT_OF_TURNS)
+            {
+                var currentNumberIndex = _numberIndexCollection[lastAdded];
+                lastAdded = (currentNumberIndex[1] != -1) ? currentNumberIndex[1] - currentNumberIndex[0] : 0;
+                AddToNumberIndexCollection(lastAdded, currentTurn++);
+            }
+
+            var result2 = lastAdded.ToString();
+            Console.WriteLine($"Solution 2 solved in {sw.Elapsed}.");
+
+            return lastAdded.ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            const int AMOUNT_OF_TURNS = 30000000;
+
+            var sw = Stopwatch.StartNew();
+
+            var currentTurn = 1;
+            _numberIndexCollection = new Dictionary<int, int[]>();
+            _startingNumbers.ForEach(number => _numberIndexCollection.Add(number, new int[2] { currentTurn++, -1 }));
+
+            var lastAdded = _startingNumbers.Last();
+            while (currentTurn <= AMOUNT_OF_TURNS)
+            {
+                var currentNumberIndex = _numberIndexCollection[lastAdded];
+                lastAdded = (currentNumberIndex[1] != -1) ? currentNumberIndex[1] - currentNumberIndex[0] : 0;
+                AddToNumberIndexCollection(lastAdded, currentTurn++);
+            }
+
+            Console.WriteLine($"Part 2 solved in {sw.Elapsed}.");
+
+            return lastAdded.ToString();
+        }
+
+        private void AddToNumberIndexCollection(int number, int currentTurn)
+        {
+            if (_numberIndexCollection.ContainsKey(number))
+            {
+                _numberIndexCollection[number] = new int[] { _numberIndexCollection[number].Max(), currentTurn++ };
+            }
+            else
+            {
+                _numberIndexCollection.Add(number, new int[] { currentTurn++, -1 });
+            }
         }
 
         private int GetNextNumber()
@@ -51,26 +116,38 @@ namespace AdventOfCode.Solutions.Year2020
                 : 0;
         }
 
-        protected override string SolvePartTwo()
-        {
-            return null;
-        }
-
         private void AssertDebugInputsPart1()
         {
-            AssertListAndAnswer(new List<int> { 0, 3, 6 }, "436");
-            AssertListAndAnswer(new List<int> { 1, 3, 2 }, "1");
-            AssertListAndAnswer(new List<int> { 2, 1, 3 }, "10");
-            AssertListAndAnswer(new List<int> { 1, 2, 3 }, "27");
-            AssertListAndAnswer(new List<int> { 2, 3, 1 }, "78");
-            AssertListAndAnswer(new List<int> { 3, 2, 1 }, "438");
-            AssertListAndAnswer(new List<int> { 3, 1, 2 }, "1836");
+            AssertListAndAnswerPart1(new List<int> { 0, 3, 6 }, "436");
+            AssertListAndAnswerPart1(new List<int> { 1, 3, 2 }, "1");
+            AssertListAndAnswerPart1(new List<int> { 2, 1, 3 }, "10");
+            AssertListAndAnswerPart1(new List<int> { 1, 2, 3 }, "27");
+            AssertListAndAnswerPart1(new List<int> { 2, 3, 1 }, "78");
+            AssertListAndAnswerPart1(new List<int> { 3, 2, 1 }, "438");
+            AssertListAndAnswerPart1(new List<int> { 3, 1, 2 }, "1836");
         }
 
-        private void AssertListAndAnswer(List<int> numbers, string expectedResult)
+        private void AssertListAndAnswerPart1(List<int> numbers, string expectedResult)
         {
             _startingNumbers = numbers;
             Assert.AreEqual(SolvePartOne(), expectedResult);
+        }
+
+        private void AssertDebugInputsPart2()
+        {
+            AssertListAndAnswerPart2(new List<int> { 0, 3, 6 }, "175594");
+            AssertListAndAnswerPart2(new List<int> { 1, 3, 2 }, "2578");
+            AssertListAndAnswerPart2(new List<int> { 2, 1, 3 }, "3544142");
+            AssertListAndAnswerPart2(new List<int> { 1, 2, 3 }, "261214");
+            AssertListAndAnswerPart2(new List<int> { 2, 3, 1 }, "6895259");
+            AssertListAndAnswerPart2(new List<int> { 3, 2, 1 }, "18");
+            AssertListAndAnswerPart2(new List<int> { 3, 1, 2 }, "362");
+        }
+
+        private void AssertListAndAnswerPart2(List<int> numbers, string expectedResult)
+        {
+            _startingNumbers = numbers;
+            Assert.AreEqual(SolvePartTwo(), expectedResult);
         }
     }
 }
