5a3828b978f1a1412b5bbd185667333bbebd41d8
Youri Bijen <YouriBijen@hotmail.com>
2020-12-16 17:47:00 +0100

    Resolved day 16 part 2

--- AdventOfCode/Solutions/Year2020/Day16/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day16/Solution.cs
@@ -1,12 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AdventOfCode.Solutions.Year2020.Day16.Models;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day16Solution : ASolution
     {
         private List<TicketField> _ticketField = new List<TicketField>();
@@ -20,15 +17,69 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            var min = _ticketField.Aggregate((v1, v2) => v1.Range1.Min < v2.Range1.Min ? v1 : v2).Range1.Min;
-            var max = _ticketField.Aggregate((v1, v2) => v1.Range2.Max > v2.Range2.Max ? v1 : v2).Range2.Max;
+            var min = GetMinimumValue();
+            var max = GetMaximumValue();
             return _tickets.SelectMany(t => t).Where(t => t < min || t > max).Sum().ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            // All the valid tickets
+            var validTickets = GetValidTickets();
+
+            // All the valid tickets, but now grouped by the index of each ticket
+            // [0] contains all the first tickets of all valid tickets
+            // [1] contains all the second tickets of all valid tickets
+            // etc...
+            var validTicketsPerField = new List<List<int>>();
+            for (var i = 0; i < _ticketField.Count; i++)
+            {
+                var ticketsPerFieldList = new List<int>();
+                for(var j = 0; j < validTickets.Count; j++)
+                {
+                    ticketsPerFieldList.Add(validTickets[j][i]);
+                }
+                validTicketsPerField.Add(ticketsPerFieldList);
+            }
+
+            // Go through all ticket indexes and find which one maps to each title
+            var mapping = _ticketField.ToDictionary(ticket => ticket.Name, ticket => new List<int>());
+            for(var i = 0; i < _ticketField.Count; i++)
+            {
+                var currentTicketField = _ticketField[i];
+
+                for(var j = 0; j < validTicketsPerField.Count; j++)
+                {
+                    if(validTicketsPerField[j].All(t =>
+                            t >= currentTicketField.Range1.Min && t <= currentTicketField.Range1.Max
+                            || t >= currentTicketField.Range2.Min && t <= currentTicketField.Range2.Max))
+                    {
+                        mapping[currentTicketField.Name].Add(j);
+                    }
+                }
+            }
+
+            // Now clean up the mapping by removing values used by multiple tickets
+            var cleanedMapping = new Dictionary<string, int>();
+            foreach(var map in mapping.OrderBy(m => m.Value.Count))
+            {
+                cleanedMapping.Add(map.Key, map.Value.FirstOrDefault(v => !cleanedMapping.ContainsValue(v)));
+            }
+
+            return cleanedMapping.Where(kvp => kvp.Key.Contains("departure"))
+                .Select(kvp => _myTicket.ElementAt(kvp.Value))
+                .Aggregate(1L, (t1, t2) => t1 * t2).ToString();
         }
+
+        private List<List<int>> GetValidTickets()
+        {
+            var min = GetMinimumValue();
+            var max = GetMaximumValue();
+            return _tickets.Where(ticket => ticket.All(t => t >= min && t <= max)).ToList();
+        }
+
+        private int GetMinimumValue() => _ticketField.Aggregate((v1, v2) => v1.Range1.Min < v2.Range1.Min ? v1 : v2).Range1.Min;
+        private int GetMaximumValue() => _ticketField.Aggregate((v1, v2) => v1.Range2.Max > v2.Range2.Max ? v1 : v2).Range2.Max;
 
         private void ParseInput()
         {
@@ -44,7 +95,7 @@ namespace AdventOfCode.Solutions.Year2020
                 {
                     Name = field.Split(":")[0],
                     Range1 = (int.Parse(ranges[0].Split("-")[0]), int.Parse(ranges[0].Split("-")[1])),
-                    Range2 = (int.Parse(ranges[0].Split("-")[1]), int.Parse(ranges[1].Split("-")[1]))
+                    Range2 = (int.Parse(ranges[1].Split("-")[0]), int.Parse(ranges[1].Split("-")[1]))
                 });
             }
 
