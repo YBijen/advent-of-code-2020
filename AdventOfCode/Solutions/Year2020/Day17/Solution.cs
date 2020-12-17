a27ea796a8e68eadfc8f5b357e254fa6d209e240
Youri Bijen <YouriBijen@hotmail.com>
2020-12-17 16:38:55 +0100

    Resolved day 17 part 2

--- AdventOfCode/Solutions/Year2020/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day17/Solution.cs
@@ -8,7 +8,9 @@ namespace AdventOfCode.Solutions.Year2020
 {
     class Day17Solution : ASolution
     {
-        private HashSet<(int X, int Y, int Z)> _pocketDimension;
+        private const int CYCLES = 6;
+        private HashSet<(int X, int Y, int Z)> _pocketDimensionCubes;
+        private HashSet<(int X, int Y, int Z, int W)> _pocketDimensionHyperCubes;
 
         public Day17Solution() : base(17, 2020, "")
         {
@@ -18,22 +20,11 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            PerformCycles(6);
-            return _pocketDimension.Count.ToString();
-        }
-
-        protected override string SolvePartTwo()
-        {
-            return null;
-        }
-
-        private void PerformCycles(int cycles)
-        {
             HashSet<(int X, int Y, int Z)> temporaryPocketDimension = new HashSet<(int X, int Y, int Z)>();
-            for (var i = 0; i < cycles; i++)
+            for (var i = 0; i < CYCLES; i++)
             {
                 temporaryPocketDimension.Clear();
-                var borders = GetCurrentBorders();
+                var borders = GetCubeBorders();
                 for (var x = borders.X.Min; x <= borders.X.Max; x++)
                 {
                     for (var y = borders.Y.Min; y <= borders.Y.Max; y++)
@@ -42,37 +33,38 @@ namespace AdventOfCode.Solutions.Year2020
                         {
                             var currentCube = (x, y, z);
                             var activeNeighbourCount = CountActiveNeighboursForCube(currentCube);
-                            if(activeNeighbourCount == 3)
+                            if (activeNeighbourCount == 3)
                             {
                                 temporaryPocketDimension.Add(currentCube);
                             }
-                            else if (activeNeighbourCount == 2 && _pocketDimension.Contains(currentCube))
+                            else if (activeNeighbourCount == 2 && _pocketDimensionCubes.Contains(currentCube))
                             {
                                 temporaryPocketDimension.Add(currentCube);
                             }
                         }
                     }
                 }
-                _pocketDimension = new HashSet<(int X, int Y, int Z)>(temporaryPocketDimension);
+                _pocketDimensionCubes = new HashSet<(int X, int Y, int Z)>(temporaryPocketDimension);
             }
+            return _pocketDimensionCubes.Count.ToString();
         }
 
         private int CountActiveNeighboursForCube((int X, int Y, int Z) cube)
         {
             var count = 0;
-            for(var x = -1; x <= 1; x++)
+            for (var x = -1; x <= 1; x++)
             {
                 for (var y = -1; y <= 1; y++)
                 {
                     for (var z = -1; z <= 1; z++)
                     {
-                        if(x == 0 && y == 0 && z == 0)
+                        if (x == 0 && y == 0 && z == 0)
                         {
                             continue;
                         }
 
                         var cubeToCheck = (cube.X + x, cube.Y + y, cube.Z + z);
-                        if(_pocketDimension.Contains(cubeToCheck))
+                        if (_pocketDimensionCubes.Contains(cubeToCheck))
                         {
                             count++;
                         }
@@ -82,10 +74,10 @@ namespace AdventOfCode.Solutions.Year2020
             return count;
         }
 
-        private (MinMax X, MinMax Y, MinMax Z) GetCurrentBorders()
+        private (MinMax X, MinMax Y, MinMax Z) GetCubeBorders()
         {
             MinMax x = new MinMax(), y = new MinMax(), z = new MinMax();
-            foreach(var cube in _pocketDimension)
+            foreach (var cube in _pocketDimensionCubes)
             {
                 UpdateMinMax(ref x, cube.X);
                 UpdateMinMax(ref y, cube.Y);
@@ -104,6 +96,94 @@ namespace AdventOfCode.Solutions.Year2020
             return (x, y, z);
         }
 
+        protected override string SolvePartTwo()
+        {
+            HashSet<(int X, int Y, int Z, int W)> temporaryPocketDimension = new HashSet<(int X, int Y, int Z, int W)>();
+            for (var i = 0; i < CYCLES; i++)
+            {
+                temporaryPocketDimension.Clear();
+                var borders = GetHyperCubeBorders();
+                for (var x = borders.X.Min; x <= borders.X.Max; x++)
+                {
+                    for (var y = borders.Y.Min; y <= borders.Y.Max; y++)
+                    {
+                        for (var z = borders.Z.Min; z <= borders.Z.Max; z++)
+                        {
+                            for (var w = borders.W.Min; w <= borders.W.Max; w++)
+                            {
+                                var currentCube = (x, y, z, w);
+
+                                var activeNeighbourCount = CountActiveNeighboursForHyperCube(currentCube);
+                                if (activeNeighbourCount == 3)
+                                {
+                                    temporaryPocketDimension.Add(currentCube);
+                                }
+                                else if (activeNeighbourCount == 2 && _pocketDimensionHyperCubes.Contains(currentCube))
+                                {
+                                    temporaryPocketDimension.Add(currentCube);
+                                }
+                            }
+                        }
+                    }
+                }
+                _pocketDimensionHyperCubes = new HashSet<(int X, int Y, int Z, int W)>(temporaryPocketDimension);
+            }
+            return _pocketDimensionHyperCubes.Count.ToString();
+        }
+
+        private int CountActiveNeighboursForHyperCube((int X, int Y, int Z, int W) cube)
+        {
+            var count = 0;
+            for (var x = -1; x <= 1; x++)
+            {
+                for (var y = -1; y <= 1; y++)
+                {
+                    for (var z = -1; z <= 1; z++)
+                    {
+                        for (var w = -1; w <= 1; w++)
+                        {
+                            if (x == 0 && y == 0 && z == 0 && w == 0)
+                            {
+                                continue;
+                            }
+
+                            var cubeToCheck = (cube.X + x, cube.Y + y, cube.Z + z, cube.W + w);
+                            if (_pocketDimensionHyperCubes.Contains(cubeToCheck))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        private (MinMax X, MinMax Y, MinMax Z, MinMax W) GetHyperCubeBorders()
+        {
+            MinMax x = new MinMax(), y = new MinMax(), z = new MinMax(), w = new MinMax();
+            foreach (var cube in _pocketDimensionHyperCubes)
+            {
+                UpdateMinMax(ref x, cube.X);
+                UpdateMinMax(ref y, cube.Y);
+                UpdateMinMax(ref z, cube.Z);
+                UpdateMinMax(ref w, cube.W);
+            }
+
+            // Update the borders to add a +1
+            // This forces a check to extra cubes which may become active
+            x.Min--;
+            x.Max++;
+            y.Min--;
+            y.Max++;
+            z.Min--;
+            z.Max++;
+            w.Min--;
+            w.Max++;
+
+            return (x, y, z, w);
+        }
+
         private void UpdateMinMax(ref MinMax minMax, int value)
         {
             if(value < minMax.Min)
@@ -118,13 +198,15 @@ namespace AdventOfCode.Solutions.Year2020
 
         private void ParseInput()
         {
-            _pocketDimension = base.Input.SplitByNewline()
+            _pocketDimensionCubes = base.Input.SplitByNewline()
                 .SelectMany((line, y) =>
                     line.Select((cube, x) => (X: x, Y: y, IsActive: cube == '#'))
                 )
                 .Where(cube => cube.IsActive)
                 .Select(cube => (cube.X, cube.Y, Z: 0))
                 .ToHashSet();
+
+            _pocketDimensionHyperCubes = _pocketDimensionCubes.Select(cube => (cube.X, cube.Y, cube.Z, 0)).ToHashSet();
         }
     }
 }
