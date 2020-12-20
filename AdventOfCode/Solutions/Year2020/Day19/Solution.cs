2866e25b06ea435e6c2d74153b8a80ebb7ebcef4
Youri Bijen <YouriBijen@hotmail.com>
2020-12-20 08:57:09 +0100

    WIP Day 19

--- AdventOfCode/Solutions/Year2020/Day19/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day19/Solution.cs
@@ -39,32 +39,63 @@ namespace AdventOfCode.Solutions.Year2020
                 // Voor elke rule die nog niet processed is
                 foreach (var rule in _rules.Where(r => !_processedRules.ContainsKey(r.Key)))
                 {
+                    // Haal de startwaarden op van de eerste value
+                    var subruleResult = new List<string>();
+
                     // Als alle subrules van de rule processed zijn
                     if (rule.Value.Item1.All(subrule => _processedRules.ContainsKey(subrule)))
                     {
-                        // Haal de startwaarden op van de eerste value
-                        var subruleResult = new List<string>(_processedRules[rule.Value.Item1[0]]);
+                        var set1Result = new List<string>(_processedRules[rule.Value.Item1[0]]);
 
                         // rule.value.item1 bevat { 4, 5 }
                         foreach (var subrule in rule.Value.Item1.Skip(1))
                         {
-                            foreach (var processedSubruleResult in _processedRules[subrule])
+                            var newSet = new List<string>();
+
+                            for(var i = 0; i < set1Result.Count; i++)
                             {
-                                // Bevat { "a" } of { "b" }
-                                for (var i = 0; i < subruleResult.Count; i++)
+                                var currValue = set1Result[i];
+                                foreach (var processedSubruleResult in _processedRules[subrule])
                                 {
-                                    subruleResult[i] += processedSubruleResult;
+                                    // Bevat { "a" } of { "b" }
+                                    newSet.Add(currValue += processedSubruleResult);
                                 }
                             }
 
+                            set1Result = new List<string>(newSet);
                         }
 
-                        _processedRules.Add(rule.Key, subruleResult);
+                        subruleResult.AddRange(set1Result);
                     }
 
-                    if (rule.Value.Item2 != null)
+                    if (rule.Value.Item2 != null && rule.Value.Item2.All(subrule => _processedRules.ContainsKey(subrule)))
                     {
-                        // Do it again
+                        var set2Result = new List<string>(_processedRules[rule.Value.Item2[0]]);
+
+                        // rule.value.item2 bevat { 4, 5 }
+                        foreach (var subrule in rule.Value.Item2.Skip(1))
+                        {
+                            var newSet = new List<string>();
+
+                            for (var i = 0; i < set2Result.Count; i++)
+                            {
+                                var currValue = set2Result[i];
+                                foreach (var processedSubruleResult in _processedRules[subrule])
+                                {
+                                    // Bevat { "a" } of { "b" }
+                                    newSet.Add(currValue += processedSubruleResult);
+                                }
+                            }
+
+                            set2Result = new List<string>(newSet);
+                        }
+
+                        subruleResult.AddRange(set2Result);
+                    }
+
+                    if(subruleResult.Count > 0)
+                    {
+                        _processedRules.Add(rule.Key, subruleResult);
                     }
                 }
             }
@@ -94,19 +125,31 @@ namespace AdventOfCode.Solutions.Year2020
         private void AssertDebugInput()
         {
             base.DebugInput = "" +
-                "0: \"a\"\n" +
-                "1: \"b\"\n" +
-                "2: 0 1 3 4 5 6\n" +
-                "3: 0\n" +
-                "4: 0 1\n" +
-                "5: 0 1 0 1\n" +
-                "6: 0 1 0 1 0 1";
+                "0: 1 2\n" +
+                "1: \"a\"\n" +
+                "2: 1 3 | 3 1\n" +
+                "3: \"b\"";
+
+            //base.DebugInput = "" +
+            //    "0: 2 1\n" +
+            //    "1: \"a\"\n" +
+            //    "2: 1 3 | 3 1\n" +
+            //    "3: \"b\"";
+
+            //base.DebugInput = "" +
+            //    "0: \"a\"\n" +
+            //    "1: \"b\"\n" +
+            //    "2: 0 1 3 4 5 6\n" +
+            //    "3: 0\n" +
+            //    "4: 0 1\n" +
+            //    "5: 0 1 0 1\n" +
+            //    "6: 0 1 0 1 0 1";
 
             Initialize(true);
 
-            CollectionAssert.AreEqual(_processedRules[2], new List<string> { "abaabababababab" });
+            //CollectionAssert.AreEqual(_processedRules[2], new List<string> { "abaabababababab" });
 
-            Console.WriteLine($"Debug input is valid!");
+            //Console.WriteLine($"Debug input is valid!");
         }
 
         private void FillMessagesFromInput()
