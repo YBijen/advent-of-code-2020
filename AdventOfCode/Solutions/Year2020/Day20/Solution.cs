8173422b7448e9d43701f0554916302699f028d3
Youri Bijen <youribijen@hotmail.com>
2020-12-23 16:55:35 +0100

    Working Full Image filler with correct image in correct rotation and flip

--- AdventOfCode/Solutions/Year2020/Day20/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day20/Solution.cs
@@ -15,18 +15,19 @@ namespace AdventOfCode.Solutions.Year2020
         private const char FILLED = '#';
 
         private const int LENGTH_IMAGE = 10;
-        private Dictionary<int, (List<char> Standard, List<char> Flipped)> _images;
-        private readonly Dictionary<int, List<char>> _validLists = new Dictionary<int, List<char>>();
-        private readonly Dictionary<(int X, int Y), int> _correctImages = new Dictionary<(int X, int Y), int>();
+        private readonly int _lengthAllImages; 
 
-        private List<(int, List<char>)> _fullImage = new List<(int, List<char>)>();
+        private Dictionary<int, (List<char> Standard, List<char> Flipped)> _images;
+        private List<(int ImageId, List<char> Image)> _fullImage = new List<(int, List<char>)>();
 
         public Day20() : base(20, 2020, "")
         {
             SetDebugInput();
             ParseInput();
-            
-            if(base.DebugInput == null)
+
+            _lengthAllImages = base.DebugInput == null ? 12 : 3; // TODO: Resolve by code
+
+            if (base.DebugInput == null)
             {
                 Assert.AreEqual(2699020245973.ToString(), SolvePartOne());
             }
@@ -44,77 +45,73 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            var lengthAllImages = base.DebugInput == null ? 12 : 3; // TODO: Resolve by code
-
-            var solvables = FindSolvesForEachImage();
-
-            var topLeftKey = FindTopLeftStartingTile(solvables);
-            _correctImages.Add((0, 0), topLeftKey);
-
-            _fullImage.Add((topLeftKey, _images[topLeftKey].Standard));
-
-            // TODO: Update the 1 after the first row works
-            for(var y = 0; y < 1; y++)
-            {
-                for(var x = 0; x < lengthAllImages; x++)
-                {
-                    // Skip top left, because we have it already
-                    if(x == 0 && x == 0)
-                    {
-                        continue;
-                    }
-
-                    // Look for y - 1 for the correct image to check
-                    if(x == 0)
-                    {
-                        // Not needed yet
-                    }
-
-                    var lastImage = _correctImages.Last();
-                    var solves = solvables[lastImage.Value];
-
-                    if(solves.Any(s => s.Rotation == 270)) // TODO: Improve
-                    {
-                        var correctSolve = solves.Find(s => s.Rotation == 270);
-                        if(correctSolve.IsFlipped)
-                        {
-                            _fullImage.Add((correctSolve.SolvableByImage, RotateImage(_images[correctSolve.SolvableByImage].Flipped, correctSolve.Rotation)));
-                        }
-                        else
-                        {
-                            _fullImage.Add((correctSolve.SolvableByImage, RotateImage(_images[correctSolve.SolvableByImage].Standard, correctSolve.Rotation)));
-                        }
-                        _correctImages.Add((x, y), correctSolve.SolvableByImage);
-                    }
-                    else
-                    {
-                        var correctSolve = solves.Find(s => s.Rotation == 90);
-                        if (correctSolve.IsFlipped)
-                        {
-                            _fullImage.Add((correctSolve.SolvableByImage, RotateImage(_images[correctSolve.SolvableByImage].Standard, correctSolve.Rotation)));
-                        }
-                        else
-                        {
-                            _fullImage.Add((correctSolve.SolvableByImage, RotateImage(_images[correctSolve.SolvableByImage].Flipped, correctSolve.Rotation)));
-                        }
-                        _correctImages.Add((x, y), correctSolve.SolvableByImage);
-
-                    }
-                }
-            }
-
-            foreach(var x in _fullImage)
-            {
-                PrintImage(x.Item1, x.Item2);
-            }
-
+            FillFullImage();
             return null;
+        }
+
+        private void FillFullImage()
+        {
+            var imageSolves = FindSolvesForEachImage();
+
+            // Initialize full image
+            var topLeftImageId = FindTopLeftStartingTile(imageSolves);
+            _fullImage.Add((topLeftImageId, _images[topLeftImageId].Standard));
+
+            for (var i = 1; i < (_lengthAllImages * _lengthAllImages); i++)
+            {
+                ImageMatch previousImageSolve;
+                string borderToFind;
+                int toRotate = 0;
+                Border borderToTake;
+
+                // If the next image to add is on a new y
+                if (i % _lengthAllImages == 0)
+                {
+                    var (imageId, image) = _fullImage[i - _lengthAllImages];
+                    borderToFind = GetBorderForImage(image, Border.Bottom);
+                    previousImageSolve = imageSolves[imageId].Find(i => i.MatchedAtRotation == 180);
+                    borderToTake = Border.Top;
+
+                    // Find the value to which the image should be rotated
+                    toRotate = Math.Abs(180 - previousImageSolve.Rotation);
+
+                }
+                // If the next image to add is on the same y
+                else
+                {
+                    var (imageId, image) = _fullImage.Last();
+                    borderToFind = GetBorderForImage(image, Border.Right);
+                    previousImageSolve = imageSolves[imageId].Find(i => i.MatchedAtRotation == 270);
+                    borderToTake = Border.Left;
+
+                    // Find the value to which the image should be rotated
+                    toRotate = Math.Abs(90 - previousImageSolve.Rotation);
+                }
+
+                var standardImage = RotateImage(_images[previousImageSolve.ImageId].Standard, toRotate);
+                if (borderToFind == GetBorderForImage(standardImage, borderToTake))
+                {
+                    _fullImage.Add((previousImageSolve.ImageId, standardImage));
+                    continue;
+                }
+
+                var flippedImage = RotateImage(_images[previousImageSolve.ImageId].Flipped, toRotate);
+                if (borderToFind == GetBorderForImage(flippedImage, borderToTake))
+                {
+                    _fullImage.Add((previousImageSolve.ImageId, flippedImage));
+                    continue;
+                }
+
+                throw new Exception($"Something is going wrong in deciding the correct rotation for the current image.");
+            }
+
+            PrintFullImage();
         }
 
         /// <summary>
         /// Find the starting tile, we're looking for a tile in the top-left corner
         /// </summary>
-        private int FindTopLeftStartingTile(Dictionary<int, List<(int SolvableByImage, int Rotation, bool IsFlipped)>> solvables)
+        private int FindTopLeftStartingTile(Dictionary<int, List<ImageMatch>> solvables)
         {
             // For the top-left corner we need a solve for rotation 90 and rotation 180
             var expectedSolvedRotation = new List<int> { 90, 180 };
@@ -132,12 +129,12 @@ namespace AdventOfCode.Solutions.Year2020
         /// For each image, count how many times it can be solved
         /// Returns a list of the possible solves with a rotation
         /// </summary>
-        private Dictionary<int, List<(int SolvableByImage, int Rotation, bool IsFlipped)>> FindSolvesForEachImage()
+        private Dictionary<int, List<ImageMatch>> FindSolvesForEachImage()
         {
-            var solvables = new Dictionary<int, List<(int SolvableByImage, int Rotation, bool IsFlipped)>>();
+            var solvables = new Dictionary<int, List<ImageMatch>>();
             foreach(var baseImage in _images)
             {
-                solvables.Add(baseImage.Key, new List<(int SolvableByImage, int Rotation, bool IsFlipped)>());
+                solvables.Add(baseImage.Key, new List<ImageMatch>());
 
                 var baseTileTopRows = new List<string>();
                 for (var r = 0; r <= 270; r += 90)
@@ -154,17 +151,20 @@ namespace AdventOfCode.Solutions.Year2020
 
                     for(var r = 0; r <= 270; r += 90)
                     {
+                        var matchedAtRotation = 0;
                         foreach(var topRow in baseTileTopRows)
                         {
                             if(topRow == GetTopRowForRotatedImage(compareTile.Value.Standard, r))
                             {
-                                solvables[baseImage.Key].Add((compareTile.Key, r, false));
+                                solvables[baseImage.Key].Add(new ImageMatch(compareTile.Key, false, r, matchedAtRotation));
                             }
 
                             if (topRow == GetTopRowForRotatedImage(compareTile.Value.Flipped, r))
                             {
-                                solvables[baseImage.Key].Add((compareTile.Key, r, true));
+                                solvables[baseImage.Key].Add(new ImageMatch(compareTile.Key, false, r, matchedAtRotation));
                             }
+
+                            matchedAtRotation += 90;
                         }
                     }
                 }
@@ -186,10 +186,19 @@ namespace AdventOfCode.Solutions.Year2020
             _ => throw new Exception("Invalid rotation value: " + r)
         };
 
-        private List<char> RotateImage(List<char> image, int rotation)
+        private static List<char> RotateImage(List<char> image, int rotation) =>
+            Enumerable.Range(0, image.Count)
+            .Select(x => image[CalcIndexForRotation(rotation, x % LENGTH_IMAGE, x / LENGTH_IMAGE)])
+            .ToList();
+
+        private static string GetBorderForImage(List<char> image, Border border) => border switch
         {
-            return Enumerable.Range(0, image.Count).Select(x => image[CalcIndexForRotation(rotation, x % LENGTH_IMAGE, x / LENGTH_IMAGE)]).ToList();
-        }
+            Border.Top => string.Join("", image.Take(LENGTH_IMAGE)),
+            Border.Right => string.Join("", image.Where((c, idx) => idx > 0 && idx % LENGTH_IMAGE == (LENGTH_IMAGE - 1))),
+            Border.Left => string.Join("", image.Where((c, idx) => idx % LENGTH_IMAGE == 0)),
+            Border.Bottom => string.Join("", image.Skip((LENGTH_IMAGE * LENGTH_IMAGE) - LENGTH_IMAGE)),
+            _ => throw new Exception("Invalid border")
+        };
 
         private string GetTopRowForRotatedImage(List<char> image, int rotation) =>
             string.Join("", Enumerable.Range(0, LENGTH_IMAGE).Select(x => image[CalcIndexForRotation(rotation, x, 0)]));
@@ -218,6 +227,24 @@ namespace AdventOfCode.Solutions.Year2020
             Console.WriteLine();
         }
 
+        private void PrintFullImage()
+        {
+            Console.WriteLine("The full image (including borders):");
+            var fullImages = _fullImage.Select(fi => fi.Image).ToList();
+            for (var y = 0; y < _lengthAllImages * LENGTH_IMAGE; y++)
+            {
+                var listIndexY = y / LENGTH_IMAGE;
+                var listY = y % LENGTH_IMAGE;
+                for (var x = 0; x < _lengthAllImages * LENGTH_IMAGE; x++)
+                {
+                    var listIndexX = x / LENGTH_IMAGE;
+                    var listX = x % LENGTH_IMAGE;
+                    Console.Write(fullImages[(listIndexY * _lengthAllImages + listIndexX)][CalcIndexForRotation(0, listX, listY)]);
+                }
+                Console.WriteLine();
+            }
+        }
+
         private void ParseInput()
         {
             _images = new Dictionary<int, (List<char> Standard, List<char> Flipped)>();
@@ -240,5 +267,29 @@ namespace AdventOfCode.Solutions.Year2020
             using var reader = new StreamReader(stream);
             base.DebugInput = reader.ReadToEnd();
         }
+    }
+
+    public class ImageMatch
+    {
+        public ImageMatch(int imageId, bool isFlipped, int rotation, int matchedAtRotation)
+        {
+            ImageId = imageId;
+            IsFlipped = isFlipped;
+            Rotation = rotation;
+            MatchedAtRotation = matchedAtRotation;
+        }
+
+        public int ImageId { get; set; }
+        public bool IsFlipped { get; set; }
+        public int Rotation { get; set; }
+        public int MatchedAtRotation { get; set; }
+    }
+
+    public enum Border
+    {
+        Top,
+        Right,
+        Bottom,
+        Left
     }
 }
