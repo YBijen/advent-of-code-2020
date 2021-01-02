fa358b47b9e8bf447497526ab6e45095469789fe
Youri Bijen <YouriBijen@hotmail.com>
2021-01-02 12:09:01 +0100

    Resolved day 23 part 1

--- AdventOfCode/Solutions/Year2020/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day23/Solution.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -17,98 +16,57 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne() => PlayGame(ParseInput(), 100);
 
-        private string PlayGame(List<int> cups, int moves)
-        {
-            var currentCupIndex = 0;
-
-            var indexModifier = 0;
-
-            for (var i = 1; i <= moves; i++)
-            {
-
-                cups = cups.Skip(indexModifier).Union(cups.Take(indexModifier)).ToList();
-                indexModifier = 0;
-                Console.WriteLine($"Round {i} start: " + string.Join(" ", cups));
-
-                var currentCup = cups[currentCupIndex];
-
-
-
-                var removedCups = GetAndRemoveNextCups(cups, currentCup);
-                var destinationCup = GetNextDestinationCup(cups, currentCup);
-
-                Console.WriteLine($"Current cup: {currentCup}; Destination cup: {destinationCup}");
-
-                var destinationIndex = cups.IndexOf(destinationCup) + 1;
-                for (var j = 0; j < removedCups.Count; j++)
-                {
-                    cups.Insert(destinationIndex + j, removedCups[j]);
-                }
-
-                Console.WriteLine("Moving: " + string.Join(" ", removedCups));
-
-
-
-
-                if (currentCupIndex > destinationIndex)
-                {
-                    indexModifier = base.Input.Length - currentCupIndex > 3 ? 3 : base.Input.Length - currentCupIndex - 1;
-                    //indexModifier = 3;
-                }
-
-                Console.WriteLine($"[CURRENT IDX] {currentCupIndex} || [DEST IDX] {destinationIndex} || {indexModifier}");
-                Console.WriteLine($"Round {i} finish: " + string.Join(" ", cups));
-
-
-
-
-                if (++currentCupIndex == 9)
-                {
-                    currentCupIndex = 0;
-                }
-                Console.WriteLine();
-            }
-
-            var indexOfLabel1 = cups.IndexOf(1);
-            return string.Join("", cups.Skip(indexOfLabel1 + 1).Union(cups.Take(indexOfLabel1)));
-        }
-
-        private List<int> GetAndRemoveNextCups(List<int> cups, int currentCup)
-        {
-            var currentCupIndex = cups.IndexOf(currentCup);
-            var takenCups = cups.Skip(currentCupIndex + 1).Take(CUPS_TO_TAKE).ToList();
-
-            // Fill cups to take from the start if some are missing
-            var i = 0;
-            while(takenCups.Count != CUPS_TO_TAKE)
-            {
-                takenCups.Add(cups[i++]);
-            }
-
-            takenCups.ForEach(tc => cups.Remove(tc));
-            return takenCups;
-        }
-
-        private int GetNextDestinationCup(List<int> cups, int currentCup)
-        {
-            var destinationCup = currentCup - 1;
-            while(true)
-            {
-                if(cups.Contains(destinationCup))
-                {
-                    return destinationCup;
-                }
-
-                if(--destinationCup <= 0)
-                {
-                    destinationCup = 9;
-                }
-            }
-        }
-
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        private string PlayGame(LinkedList<int> cups, int moves)
+        {
+            var currentCup = cups.First;
+            for (var i = 1; i <= moves; i++)
+            {
+                var removedCups = new List<int>();
+                for (var j = 0; j < CUPS_TO_TAKE; j++)
+                {
+                    removedCups.Insert(0, currentCup.Next?.Value ?? cups.First.Value);
+                    cups.Remove(currentCup?.Next ?? cups.First);
+                }
+
+                var destinationCup = GetNextDestinationCup(removedCups, currentCup.Value);
+                var destinationCupNode = cups.Find(destinationCup);
+
+                removedCups.ForEach(cup => cups.AddAfter(destinationCupNode ?? cups.First, cup));
+
+                currentCup = currentCup.Next ?? cups.First;
+            }
+
+            return CreateOutputForResult(cups);
+        }
+
+
+        private string CreateOutputForResult(LinkedList<int> cups)
+        {
+            var cupsList = cups.ToList();
+            var indexOfLabel1 = cupsList.IndexOf(1);
+            return string.Join("", cupsList.Skip(indexOfLabel1 + 1).Union(cupsList.Take(indexOfLabel1)));
+        }
+
+        private int GetNextDestinationCup(List<int> removedCups, int currentCup)
+        {
+            var destinationCupValue = currentCup;
+            while(true)
+            {
+                if (--destinationCupValue <= 0)
+                {
+                    destinationCupValue = base.Input.Length;
+                }
+
+                if (!removedCups.Contains(destinationCupValue))
+                {
+                    return destinationCupValue;
+                }
+            }
         }
 
         private void AssertPartOne()
@@ -119,6 +77,11 @@ namespace AdventOfCode.Solutions.Year2020
             base.DebugInput = null;
         }
 
-        private List<int> ParseInput() => base.Input.Select(c => int.Parse(c.ToString())).ToList();
+        private LinkedList<int> ParseInput()
+        {
+            var ll = new LinkedList<int>();
+            base.Input.Select(c => int.Parse(c.ToString())).Reverse().ToList().ForEach(v => ll.AddFirst(v));
+            return ll;
+        }
     }
 }
