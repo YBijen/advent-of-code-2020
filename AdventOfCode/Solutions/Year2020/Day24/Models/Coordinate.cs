83a2152aac475e9e185cceb85267442b5ed668c2
Youri Bijen <YouriBijen@hotmail.com>
2021-01-03 13:51:04 +0100

    Resolved day 24 part 2

--- AdventOfCode/Solutions/Year2020/Day24/Models/Coordinate.cs
+++ AdventOfCode/Solutions/Year2020/Day24/Models/Coordinate.cs
@@ -1,21 +1,39 @@
-﻿namespace AdventOfCode.Solutions.Year2020.Day24.Models
-{
-    public class Coordinate
-    {
-        public Coordinate()
-        {
+﻿using System;
 
+namespace AdventOfCode.Solutions.Year2020.Day24.Models
+{
+    public class Coordinate : IEquatable<Coordinate>
+    {
+        public Coordinate() { }
+
+        public Coordinate(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
-        public Coordinate((int X, int Y, int Z) tupleCoords)
+        public Coordinate(Coordinate coordinate)
         {
-            this.X = tupleCoords.X;
-            this.Y = tupleCoords.Y;
-            this.Z = tupleCoords.Z;
+            this.X = coordinate.X;
+            this.Y = coordinate.Y;
+            this.Z = coordinate.Z;
         }
 
         public int X { get; set; }
         public int Y { get; set; }
         public int Z { get; set; }
+
+        public bool Equals(Coordinate other) => other.X == this.X && other.Y == this.Y && other.Z == this.Z;
+
+        public override int GetHashCode()
+        {
+            return 31 * X + 17 * Y + 334 * Z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Coordinate && Equals((Coordinate)obj);
+        }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day24/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day24/Solution.cs
@@ -7,6 +7,9 @@ namespace AdventOfCode.Solutions.Year2020
 {
     class Day24Solution : ASolution
     {
+        const int DAYS = 100;
+        private HashSet<Coordinate> _flippedTiles;
+
         public Day24Solution() : base(24, 2020, "")
         {
             SetDebugInput();
@@ -16,59 +19,159 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            _flippedTiles = GetFlippedTiles(ParseInput()).ToHashSet();
+            for (var i = 1; i <= DAYS; i++)
+            {
+                var (minimum, maximum) = GetFloorBorders();
+                var newFlippedTiles = new List<Coordinate>(_flippedTiles);
+                for (var x = minimum.X; x <= maximum.X; x++)
+                {
+                    for (var y = minimum.Y; y <= maximum.Y; y++)
+                    {
+                        for (var z = minimum.Z; z <= maximum.Z; z++)
+                        {
+                            var currentTile = new Coordinate(x, y, z);
+                            var amountOfFlippedNeighbours = CountFlippedNeighbours(currentTile);
+                            if(_flippedTiles.Contains(currentTile))
+                            {
+                                if(amountOfFlippedNeighbours == 0 || amountOfFlippedNeighbours > 2)
+                                {
+                                    newFlippedTiles.Remove(currentTile);
+                                }
+                            }
+                            else
+                            {
+                                if(amountOfFlippedNeighbours == 2)
+                                {
+                                    newFlippedTiles.Add(currentTile);
+                                }
+                            }
+                        }
+                    }
+                }
+                _flippedTiles = new HashSet<Coordinate>(newFlippedTiles);
+                Console.WriteLine($"Flipped tiles after day {i}: {_flippedTiles.Count}");
+            }
+            return _flippedTiles.Count.ToString();
         }
 
-        private List<(int X, int Y, int Z)> GetFlippedTiles(List<List<Direction>> tilePaths)
+        private int CountFlippedNeighbours(Coordinate tile)
         {
-            var result = new Dictionary<(int X, int Y, int Z), int>();
+            var flippedNeighbours = 0;
+
+            foreach(var direction in (Direction[])Enum.GetValues(typeof(Direction)))
+            {
+                var neighbour = GetTileInDirection(direction, tile);
+                if(_flippedTiles.Contains(neighbour))
+                {
+                    flippedNeighbours++;
+                }
+            }
+
+            return flippedNeighbours;
+        }
+
+        private (Coordinate min, Coordinate max) GetFloorBorders()
+        {
+            Coordinate minimum = new Coordinate(), maximum = new Coordinate();
+
+            foreach(var tile in _flippedTiles)
+            {
+                if(tile.X < minimum.X)
+                {
+                    minimum.X = tile.X;
+                }
+                if (tile.X > maximum.X)
+                {
+                    maximum.X = tile.X;
+                }
+
+                if (tile.Y < minimum.Y)
+                {
+                    minimum.Y = tile.Y;
+                }
+                if (tile.Y > maximum.Y)
+                {
+                    maximum.Y = tile.Y;
+                }
+
+                if (tile.Z < minimum.Z)
+                {
+                    minimum.Z = tile.Z;
+                }
+                if (tile.Z > maximum.Z)
+                {
+                    maximum.Z = tile.Z;
+                }
+            }
+
+            minimum.X--;
+            minimum.Y--;
+            minimum.Z--;
+            maximum.X++;
+            maximum.Y++;
+            maximum.Z++;
+
+            return (minimum, maximum);
+        }
+
+        private List<Coordinate> GetFlippedTiles(List<List<Direction>> tilePaths)
+        {
+            var result = new Dictionary<Coordinate, int>();
             foreach(var tilePathDirections in tilePaths)
             {
                 var currentTile = new Coordinate();
                 foreach(var direction in tilePathDirections)
                 {
-                    switch(direction)
-                    {
-                        case Direction.NorthEast:
-                            currentTile.X += 1;
-                            currentTile.Z -= 1;
-                            break;
-                        case Direction.East:
-                            currentTile.X += 1;
-                            currentTile.Y -= 1;
-                            break;
-                        case Direction.SouthEast:
-                            currentTile.Z += 1;
-                            currentTile.Y -= 1;
-                            break;
-                        case Direction.SouthWest:
-                            currentTile.X -= 1;
-                            currentTile.Z += 1;
-                            break;
-                        case Direction.West:
-                            currentTile.X -= 1;
-                            currentTile.Y += 1;
-                            break;
-                        case Direction.NorthWest:
-                            currentTile.Z -= 1;
-                            currentTile.Y += 1;
-                            break;
-                        default:
-                            throw new Exception("This should not happen.");
-                    }
+                    currentTile = GetTileInDirection(direction, currentTile);
                 }
-                var foundTile = (currentTile.X, currentTile.Y, currentTile.Z);
 
-                if (result.ContainsKey(foundTile))
+                if (result.ContainsKey(currentTile))
                 {
-                    result[foundTile]++;
+                    result[currentTile]++;
                 }
                 else
                 {
-                    result.Add(foundTile, 1);
+                    result.Add(currentTile, 1);
                 }
             }
             return result.Where(tile => tile.Value % 2 != 0).Select(tile => tile.Key).ToList();
+        }
+
+        private Coordinate GetTileInDirection(Direction direction, Coordinate currentTile)
+        {
+            var nextTile = new Coordinate(currentTile);
+            switch (direction)
+            {
+                case Direction.NorthEast:
+                    nextTile.X += 1;
+                    nextTile.Z -= 1;
+                    break;
+                case Direction.East:
+                    nextTile.X += 1;
+                    nextTile.Y -= 1;
+                    break;
+                case Direction.SouthEast:
+                    nextTile.Z += 1;
+                    nextTile.Y -= 1;
+                    break;
+                case Direction.SouthWest:
+                    nextTile.X -= 1;
+                    nextTile.Z += 1;
+                    break;
+                case Direction.West:
+                    nextTile.X -= 1;
+                    nextTile.Y += 1;
+                    break;
+                case Direction.NorthWest:
+                    nextTile.Z -= 1;
+                    nextTile.Y += 1;
+                    break;
+                default:
+                    throw new Exception("This should not happen.");
+
+            }
+            return nextTile;
         }
 
         private List<List<Direction>> ParseInput()
